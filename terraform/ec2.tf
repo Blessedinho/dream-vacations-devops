853532e56cfce76e1e95ff07f0632198d8049fa5
oem <blessedinhotronics@gmail.com>
2026-08-25 10:53:56 +0100

    Open port 3000 in security group, add deploy job to CD pipeline, add prod compose file

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -50,6 +50,14 @@ resource "aws_security_group" "app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Frontend Dev Port"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
